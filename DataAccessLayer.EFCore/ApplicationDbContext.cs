43e959b9e5326a5382052628be855bc9d98c3aa5
David Silwal <de.davidsilwal@hotmail.com>
2020-03-25 13:52:31 +0545

    owned entity

---
+++ DataAccessLayer.EFCore/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.EFCore
+{
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Street { get; set; }
+    }
+
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+
+        public virtual DbSet<Posts> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Posts>()
+                .IsMemoryOptimized();
+
+            modelBuilder.Entity<Person>().OwnsOne(typeof(Address), "Address");
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
